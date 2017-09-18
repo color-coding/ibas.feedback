f520fb7c14fa1bae55173526f00713dd8caad498
牛加人等于朱 <niuren.zhu@icloud.com>
2017-09-18 17:39:45 +0800

    修复框架兼容问题

--- ibas.feedback/src/main/java/org/colorcoding/ibas/feedback/repository/BORepositoryFeedback.java
+++ ibas.feedback/src/main/java/org/colorcoding/ibas/feedback/repository/BORepositoryFeedback.java
@@ -1,55 +1,67 @@
 package org.colorcoding.ibas.feedback.repository;
 
-import org.colorcoding.ibas.bobas.common.*;
-import org.colorcoding.ibas.bobas.ownership.*;
-import org.colorcoding.ibas.bobas.repository.*;
-import org.colorcoding.ibas.feedback.bo.suggestion.*;
+import org.colorcoding.ibas.bobas.common.ICriteria;
+import org.colorcoding.ibas.bobas.common.IOperationResult;
+import org.colorcoding.ibas.bobas.common.OperationResult;
+import org.colorcoding.ibas.bobas.repository.BORepositoryServiceApplication;
+import org.colorcoding.ibas.feedback.bo.suggestion.ISuggestion;
+import org.colorcoding.ibas.feedback.bo.suggestion.Suggestion;
 
 /**
-* Feedback仓库
-*/
-@PermissionGroup("Feedback")
-public class BORepositoryFeedback extends BORepositoryServiceApplication implements IBORepositoryFeedbackSvc, IBORepositoryFeedbackApp {
+ * Feedback仓库
+ */
+public class BORepositoryFeedback extends BORepositoryServiceApplication
+		implements IBORepositoryFeedbackSvc, IBORepositoryFeedbackApp {
 
-    //--------------------------------------------------------------------------------------------//
-    /**
-     * 查询-建议
-     * @param criteria 查询
-     * @param token 口令
-     * @return 操作结果
-     */
-    public OperationResult<Suggestion> fetchSuggestion(ICriteria criteria, String token) {
-        return super.fetch(criteria, token, Suggestion.class);
-    }
+	// --------------------------------------------------------------------------------------------//
+	/**
+	 * 查询-建议
+	 * 
+	 * @param criteria
+	 *            查询
+	 * @param token
+	 *            口令
+	 * @return 操作结果
+	 */
+	public OperationResult<Suggestion> fetchSuggestion(ICriteria criteria, String token) {
+		return super.fetch(criteria, token, Suggestion.class);
+	}
 
-    /**
-     * 查询-建议（提前设置用户口令）
-     * @param criteria 查询
-     * @return 操作结果
-     */
-    public IOperationResult<ISuggestion> fetchSuggestion(ICriteria criteria) {
-        return new OperationResult<ISuggestion>(this.fetchSuggestion(criteria, this.getUserToken()));
-    }
+	/**
+	 * 查询-建议（提前设置用户口令）
+	 * 
+	 * @param criteria
+	 *            查询
+	 * @return 操作结果
+	 */
+	public IOperationResult<ISuggestion> fetchSuggestion(ICriteria criteria) {
+		return new OperationResult<ISuggestion>(this.fetchSuggestion(criteria, this.getUserToken()));
+	}
 
-    /**
-     * 保存-建议
-     * @param bo 对象实例
-     * @param token 口令
-     * @return 操作结果
-     */
-    public OperationResult<Suggestion> saveSuggestion(Suggestion bo, String token) {
-        return super.save(bo, token);
-    }
+	/**
+	 * 保存-建议
+	 * 
+	 * @param bo
+	 *            对象实例
+	 * @param token
+	 *            口令
+	 * @return 操作结果
+	 */
+	public OperationResult<Suggestion> saveSuggestion(Suggestion bo, String token) {
+		return super.save(bo, token);
+	}
 
-    /**
-     * 保存-建议（提前设置用户口令）
-     * @param bo 对象实例
-     * @return 操作结果
-     */
-    public IOperationResult<ISuggestion> saveSuggestion(ISuggestion bo) {
-        return new OperationResult<ISuggestion>(this.saveSuggestion((Suggestion) bo, this.getUserToken()));
-    }
+	/**
+	 * 保存-建议（提前设置用户口令）
+	 * 
+	 * @param bo
+	 *            对象实例
+	 * @return 操作结果
+	 */
+	public IOperationResult<ISuggestion> saveSuggestion(ISuggestion bo) {
+		return new OperationResult<ISuggestion>(this.saveSuggestion((Suggestion) bo, this.getUserToken()));
+	}
 
-    //--------------------------------------------------------------------------------------------//
+	// --------------------------------------------------------------------------------------------//
 
 }
