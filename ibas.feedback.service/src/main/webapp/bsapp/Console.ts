955ea6c1e5ba0f8aa81c0a9a941f515630af67a2
牛加人等于朱 <niuren.zhu@icloud.com>
2018-02-08 13:46:10 +0800

    合并业务对象的资源文件

--- ibas.feedback.service/src/main/webapp/bsapp/Console.ts
+++ ibas.feedback.service/src/main/webapp/bsapp/Console.ts
@@ -37,8 +37,7 @@ export class Console extends ibas.ModuleConsole {
     run(): void {
         // 加载语言-框架默认
         ibas.i18n.load(this.rootUrl + "resources/languages/feedback.json");
-        ibas.i18n.load(this.rootUrl + "resources/languages/enums.json");
-        ibas.i18n.load(this.rootUrl + "resources/languages/bo/suggestion.json");
+        ibas.i18n.load(this.rootUrl + "resources/languages/bos.json");
         // 设置资源属性
         this.description = ibas.i18n.prop(this.name.toLowerCase());
         this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
@@ -96,8 +95,7 @@ export class ConsoleUsers extends ibas.ModuleConsole {
     run(): void {
         // 加载语言-框架默认
         ibas.i18n.load(this.rootUrl + "resources/languages/feedback.json");
-        ibas.i18n.load(this.rootUrl + "resources/languages/bo/suggestion.json");
-        ibas.i18n.load(this.rootUrl + "resources/languages/enums.json");
+        ibas.i18n.load(this.rootUrl + "resources/languages/bos.json");
         // 设置资源属性
         this.description = ibas.i18n.prop(this.name.toLowerCase());
         this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
