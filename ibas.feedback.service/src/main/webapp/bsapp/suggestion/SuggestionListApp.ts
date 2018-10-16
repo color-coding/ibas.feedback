b2d0221d10901d6d1df0cb0a428f1bea705cb199
牛加人等于朱 <niuren.zhu@icloud.com>
2018-10-16 18:08:28 +0800

    优化代码

--- ibas.feedback.service/src/main/webapp/bsapp/suggestion/SuggestionListApp.ts
+++ ibas.feedback.service/src/main/webapp/bsapp/suggestion/SuggestionListApp.ts
@@ -146,6 +146,7 @@ namespace feedback {
                                                         ibas.i18n.prop("shell_data_delete") + ibas.i18n.prop("shell_sucessful"));
                                                 }
                                             } catch (error) {
+                                                that.busy(false);
                                                 that.messages(ibas.emMessageType.ERROR,
                                                     ibas.i18n.prop("shell_data_delete_error", beSaved, error.message));
                                             }
