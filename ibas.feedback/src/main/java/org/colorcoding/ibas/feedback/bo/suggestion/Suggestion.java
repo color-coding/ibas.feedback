aa34e2674d837461bd14ee0ffa411998eb94a22a
牛加人等于朱 <niuren.zhu@icloud.com>
2019-05-30 15:00:40 +0800

    使用BusinessObjectUnit替换BOCode

--- ibas.feedback/src/main/java/org/colorcoding/ibas/feedback/bo/suggestion/Suggestion.java
+++ ibas.feedback/src/main/java/org/colorcoding/ibas/feedback/bo/suggestion/Suggestion.java
@@ -11,7 +11,7 @@ import org.colorcoding.ibas.bobas.bo.IBOUserFields;
 import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.data.DateTime;
 import org.colorcoding.ibas.bobas.data.emYesNo;
-import org.colorcoding.ibas.bobas.mapping.BOCode;
+import org.colorcoding.ibas.bobas.mapping.BusinessObjectUnit;
 import org.colorcoding.ibas.bobas.mapping.DbField;
 import org.colorcoding.ibas.bobas.mapping.DbFieldType;
 import org.colorcoding.ibas.feedback.MyConfiguration;
@@ -23,7 +23,7 @@ import org.colorcoding.ibas.feedback.MyConfiguration;
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = Suggestion.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlRootElement(name = Suggestion.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
-@BOCode(Suggestion.BUSINESS_OBJECT_CODE)
+@BusinessObjectUnit(code = Suggestion.BUSINESS_OBJECT_CODE)
 public class Suggestion extends BusinessObject<Suggestion> implements ISuggestion, IBOUserFields {
 
 	/**
